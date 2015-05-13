d1b0a9d9a668903c4206a0bf023a836fa045630a
mkusek <mario.kusek@fer.hr>
2015-05-13 22:03:14 +0200

    Added second activity and added intent on Fetch click

--- app/src/main/java/hr/fer/croapps/croappsandroidworkshop/SecondActivity.java
+++ app/src/main/java/hr/fer/croapps/croappsandroidworkshop/SecondActivity.java
@@ -1,38 +1,24 @@
 package hr.fer.croapps.croappsandroidworkshop;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SecondActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button btnFetch = (Button) findViewById(R.id.btn_fetch);
-        btnFetch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("SCHEDULE", "Button fetch clicked!");
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_second);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_second, menu);
         return true;
     }
 

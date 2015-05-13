253bc21b831f7e0e4026e1e607748369e3cf163e
mkusek <mario.kusek@fer.hr>
2015-05-13 23:59:38 +0200

    Calling activity from FER Droid

--- app/src/main/java/hr/fer/croapps/croappsandroidworkshop/ScheduleListActivity.java
+++ app/src/main/java/hr/fer/croapps/croappsandroidworkshop/ScheduleListActivity.java
@@ -1,11 +1,15 @@
 package hr.fer.croapps.croappsandroidworkshop;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -26,4 +30,21 @@ public class ScheduleListActivity extends ListActivity {
         setListAdapter(new ScheduleListAdapter(this, schedule));
         getListView().setTextFilterEnabled(true);
     }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id)
+    {
+        super.onListItemClick(l, v, position, id);
+
+        Object o = this.getListAdapter().getItem(position);
+        ScheduleEntry entry = (ScheduleEntry) o;
+
+        String action = "hr.fer.projekt.android.contacts.map.ROOM";
+        String hotel = "ferdroid://room/" + entry.getRoom();
+        Uri data = Uri.parse(hotel);
+
+        Intent intent = new Intent(action, data);
+        startActivity(intent);
+    }
+
 }
